d50b56888fd3c001066ad97f081f23692fb3c2a5
Simon Rodriguez <kosua20@gmail.com>
2020-08-13 19:44:16 +0200

    Add PNG and ZLIB.

--- configure.sh
+++ configure.sh
@@ -1,6 +1,13 @@
 cd external
-mkdir install
+mkdir -p install
 
+mkdir -p glfw_build
+cd glfw_build
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX="../install/" ../glfw/
+cmake --build ./ --config Release
+cmake --install ./
+
+cd ..
 mkdir -p string_theory_build
 cd string_theory_build
 cmake -DCMAKE_BUILD_TYPE=Release -DST_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX="../install/" ../string_theory/
@@ -15,15 +22,22 @@ cmake --build ./ --config Release
 cmake --install ./
 
 cd ..
-mkdir -p libhsplasma_build
-cd libhsplasma_build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../install/lib/cmake/string_theory;../install/lib/pkgconfig/;/usr/local/opt/openssl" -DENABLE_PYTHON=OFF -DENABLE_TOOLS=OFF -DENABLE_NET=OFF -DENABLE_PHYSX=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="../install/" ../libhsplasma/
+mkdir -p zlib_build
+cd zlib_build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../install/" ../zlib/
 cmake --build ./ --config Release
 cmake --install ./
 
 cd ..
-mkdir -p glfw_build
-cd glfw_build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX="../install/" ../glfw/
+mkdir -p libpng_build
+cd libpng_build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../install/share/pkgconfig/;../install/" -DCMAKE_INSTALL_PREFIX="../install/" ../libpng/
+cmake --build ./ --config Release
+cmake --install ./
+
+cd ..
+mkdir -p libhsplasma_build
+cd libhsplasma_build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../install/share/pkgconfig/;../install/;../install/lib/;../install/lib/libpng/;../install/lib/cmake/string_theory;../install/lib/pkgconfig/;/usr/local/opt/openssl" -DENABLE_PYTHON=OFF -DENABLE_TOOLS=OFF -DENABLE_NET=OFF -DENABLE_PHYSX=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="../install/" ../libhsplasma/
 cmake --build ./ --config Release
 cmake --install ./
